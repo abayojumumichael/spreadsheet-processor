e36af7d632499f0cff27f8024cb8f797b25d1513
Michael Abayomi-Ojumu <abayojumumichael@gmail.com>
2025-05-28 09:14:53 +0100

    Developed Main

---
+++ Main.java
@@ -0,0 +1,13 @@
+/**
+ * Entry point for the spreadsheet processor application.
+ */
+public class Main {
+    public static void main(String[] args) {
+        /**
+        * Main method to start the application.
+        * @param args command-line arguments
+        */
+        UserInterface ui = new UserInterface();
+        ui.run();
+    }
+}
